84c95a9e4a00e5b7d8ea6d219892a39477298ff5
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-01 18:26:02 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Receba um número inteiro informado pelo usuário. Realize a decomposição decimal em unidades, dezenas e centenas.
+
+int numero;
+int unidades;
+int dezenas;
+int centenas;
+
+
+
+Console.WriteLine("---Decomposição Decimal---\n");
+
+Console.Write("Digite um número inteiro: ");
+numero = int.Parse(Console.ReadLine()!);
+
+unidades = numero % 10;
+dezenas = (numero / 10) % 10;
+centenas = numero / 100;
+
+Console.WriteLine($"O número digitado foi {numero}\n");
+Console.WriteLine($"Centenas: {centenas}\n");
+Console.WriteLine($"Dezenas: {dezenas}\n");
+Console.WriteLine($"Unidades: {unidades}\n");
+Console.ReadLine();
+
